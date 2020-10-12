7fc872529c55495ecef7b6a436b92827b875bfe6
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2020-10-12 19:49:53 +0100

    Updated src code for EnchantsAPI class
    Updated src code for EnchantsAPI class, for the 0.3-DEV version

--- EnchantsAPI.java
+++ EnchantsAPI.java
@@ -1,59 +1,51 @@
-package me.illuminatifish.enchantmentsapi.EnchantsAPI;
+package me.illuminatifish.enchantsapi;
 
-import org.bukkit.Material;
+import me.illuminatifish.enchantsapi.Utils.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import de.tr7zw.nbtapi.NBTItem;
 
+
 public class EnchantsAPI {
 
-	public static boolean hasCustomEnchant(ItemStack item, String enchantment) {
-		if(item == null || item.getType() == Material.AIR) {
-			return false;
-		} else {
-			NBTItem nbtItem = new NBTItem(item);
-			if(nbtItem.hasNBTData()) {
-				for(String key : nbtItem.getKeys()) {
-					if(key.startsWith("ae_enchantment")) {
-						String enchant = key.split(";")[1];
-						if(enchant.equalsIgnoreCase(enchantment)) {
-							return true;
-						}
-					}
-				}
-			}
-			return false;
-		}
-	}
-	
-	public static int getEnchantLevel(ItemStack item, String enchantment) {
-		if(item == null || item.getType() == Material.AIR) {
-			return -1;
-		} else {
-			NBTItem nbtItem = new NBTItem(item);
-			if(nbtItem.hasNBTData()) {
-				for(String key : nbtItem.getKeys()) {
-					if(key.startsWith("ae_enchantment")) {
-						String enchant = key.split(";")[1];
-						if(enchant.equalsIgnoreCase(enchantment)) {
-							return nbtItem.getInteger(key);
-						}
-					}
-				}
-			}
-			return 0;
-		}	
-	}
-	
-	public static int getLevelStack(Player player, String enchantment) {
-		int levelStack = 0;
-		for(ItemStack item : player.getInventory().getArmorContents()) {
-			if(EnchantsAPI.hasCustomEnchant(item, enchantment)) {
-				levelStack += EnchantsAPI.getEnchantLevel(item, enchantment);
-			}
-		}
-		return levelStack;
-	}
-	
+    public static boolean hasCustomEnchant(ItemStack item, String enchantment) {
+        if(!Utils.isNull(item)) {
+            NBTItem nbtItem = new NBTItem(item);
+            if(nbtItem.hasNBTData()) {
+                for(String key : nbtItem.getKeys()) {
+                    if(key.startsWith("ae_enchantment")) {
+                        return key.split(";")[1].equalsIgnoreCase(enchantment);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int getEnchantLevel(ItemStack item, String enchantment) {
+        if(!Utils.isNull(item)) {
+            NBTItem nbtItem = new NBTItem(item);
+            if(nbtItem.hasNBTData()) {
+                for(String key : nbtItem.getKeys()) {
+                    if(key.startsWith("ae_enchantment") &&
+                            key.split(";")[1].equalsIgnoreCase(enchantment)) {
+                        return nbtItem.getInteger(key.split(";")[1]);
+                    }
+
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int getLevelStack(Player player, String enchantment) {
+        int levelStack = 0;
+        for(ItemStack item : player.getInventory().getArmorContents()) {
+            if(EnchantsAPI.hasCustomEnchant(item, enchantment)) {
+                levelStack += EnchantsAPI.getEnchantLevel(item, enchantment);
+            }
+        }
+        return levelStack;
+    }
 }

3024ec02409f86e9f44ae6b304f18dc17ed9dd58
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-01-13 22:44:40 +0000

    Update EnchantsAPI.java
    Updated,  old method to get the level of the enchantment, due to NBT composition changing the code had to be adjusted too.

--- EnchantsAPI.java
+++ EnchantsAPI.java
@@ -30,7 +30,8 @@ public class EnchantsAPI {
                 for(String key : nbtItem.getKeys()) {
                     if(key.startsWith("ae_enchantment") &&
                             key.split(";")[1].equalsIgnoreCase(enchantment)) {
-                        return nbtItem.getInteger(key.split(";")[1]);
+                        //return nbtItem.getInteger(key.split(";")[1]); - Old method to get the level of the enchantment, due to NBT composition changing the code had to be adjusted too.
+                        return nbtItem.getInteger(key).intValue();
                     }
 
                 }
